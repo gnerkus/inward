151205fb87fe6dde2ce166cc6e749239aad5eeeb
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-08-25 20:35:52 +0200

    detect which card was clicked

--- main.lua
+++ main.lua
@@ -25,6 +25,29 @@ function love.load()
         card_module.Card.new({ 0, 0.5, 0.75, 1 }),
         card_module.Card.new({ 0, 0.25, 0.75, 1 }),
     }
+
+    whichCell = -1
+end
+
+-- return -1 if no card else card index
+function mousePosToCardIdx(posX, posY)
+    local outsideX = posX < BoardTopX or posX > BoardTopX + (BoardSize * CardWidth)
+    local outsideY = posY < BoardTopY or posY > BoardTopY + (BoardSize * CardWidth)
+
+    if outsideX or outsideY then
+        return -1
+    else
+        local column = math.ceil((posX - BoardTopX) / CardWidth)
+        local row = math.ceil((posY - BoardTopY) / CardWidth)
+
+        return (row - 1) * BoardSize + column
+    end
+end
+
+function love.mousereleased(x, y, button, istouch, presses)
+    if button == 1 then
+        whichCell = mousePosToCardIdx(x, y)
+    end
 end
 
 function love.draw()
@@ -37,4 +60,6 @@ function love.draw()
             CardWidth
         )
     end
+
+    love.graphics.print(whichCell, 0, 0)
 end
