8adea53c904c15c9852496e61aab31a6a30e37bb
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-08-31 00:56:39 +0200

    configure default font
    set background

--- main.lua
+++ main.lua
@@ -3,6 +3,9 @@ Gamestates = require "gamestates"
 Urutora = require("libs/urutora")
 UI = Urutora:new()
 
+local bg = love.graphics.newImage('img/bg4.png')
+local bgColor = { 0.1, 0.1, 0.2 }
+
 local uiStyle = {
     outline = false,
     cornerRadius = 0.5
@@ -16,6 +19,10 @@ local uiStyle = {
 function love.load()
     Gamestates.set_state("states/menu")
     UI:setStyle(uiStyle)
+    BGCanvas = love.graphics.newCanvas(1024, 640)
+    BGCanvas:setFilter('nearest', 'nearest')
+    local proggyTiny = love.graphics.newFont("fonts/proggy/ProggyTiny.ttf", 32)
+    UI.utils.default_font = proggyTiny
 end
 
 function love.update(dt)
@@ -24,7 +31,23 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.setCanvas({ canvas = BGCanvas, stencil = true })
+    love.graphics.clear(bgColor)
+    love.graphics.setColor(1, 1, 1)
+    ---draw background
+    love.graphics.draw(
+        bg,
+        BGCanvas:getWidth() / 2,
+        BGCanvas:getHeight() / 2,
+        0,
+        BGCanvas:getWidth() / bg:getWidth() * 3,
+        BGCanvas:getHeight() / bg:getHeight() * 3,
+        bg:getWidth() / 2,
+        bg:getHeight() / 2
+    )
     UI:draw()
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setCanvas()
     Gamestates.state_event("draw")
 end
 
